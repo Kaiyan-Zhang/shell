14f15ce45e5ad8330066bc348c02d95d99b95bfd
Kaiyan-Zhang <zhangkaiyan3@gmail.com>
2024-11-08 23:33:15 +0800

    feat: no powerline

---
+++ .archived/powerline/main.sh
@@ -0,0 +1,2 @@
+source ${0:A:h}/powerline.sh 
+.(zr romkatv/powerlevel10k.git/powerlevel10k.zsh-theme)
--- fancy_zsh/main.sh
+++ fancy_zsh/main.sh
@@ -1,14 +1,14 @@
 source ${0:A:h}/zr.zsh
 # source ${0:A:h}/zplug.sh
 
-# MAC
+
+# INSTALL FONT
 # brew install --cask font-meslo-lg-nerd-font
 # MesloLGM Nerd Font
 
-# powerline
-    # Apple Terminal: Terminal → Preferences → Profiles → Text
-    # Hyper: 搜索fontFamily under module.exports.config
-    # Vscode: "terminal.integrated.fontFamily": "MesloLGM Nerd Font"
-    # GNOME Terminal: Terminal → Preferences and click on the selected profile under Profiles. Check Custom font under Text Appearance and select MesloLGS NF Regular.
-    # Windows Terminal : search for: fontFace
-    source ${0:A:h}/powerline.sh 
+# SET YOUR FONT
+# Apple Terminal: Terminal → Preferences → Profiles → Text
+# Hyper: 搜索fontFamily under module.exports.config
+# Vscode: "terminal.integrated.fontFamily": "MesloLGM Nerd Font"
+# GNOME Terminal: Terminal → Preferences and click on the selected profile under Profiles. Check Custom font under Text Appearance and select MesloLGS NF Regular.
+# Windows Terminal : search for: fontFace
