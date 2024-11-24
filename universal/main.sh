2e5c98e66000ee20fa149cba075fed4507df7882
Kaiyan-Zhang <zhangkaiyan3@gmail.com>
2024-11-24 17:02:06 +0800

    feat: ofd

--- universal/main.sh
+++ universal/main.sh
@@ -5,6 +5,7 @@ source ${0:A:h}/systype.sh
 source ${0:A:h}/ls.sh
 source ${0:A:h}/cd.sh
 source ${0:A:h}/rm.sh
+source ${0:A:h}/ofd.sh
 source ${0:A:h}/lang.sh
 source ${0:A:h}/git/main.sh
 source ${0:A:h}/vscode.sh
