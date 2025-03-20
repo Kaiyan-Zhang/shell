cf4a54ebeb9e8f00dc90b9dbb57e8b70bac41fa8
Kaiyan-Zhang <zhangkaiyan3@gmail.com>
2025-03-20 21:29:16 +0800

    ultimate

--- universal/git/git.sh
+++ universal/git/git.sh
@@ -19,6 +19,19 @@ g(){
             git symbolic-ref refs/remotes/origin/HEAD | sed 's@.*/@@' # 本地版，不需要联网
             # git remote show origin | sed -n '/HEAD branch/s/.*: //p' # 远程版，需要联网
             ;;
+        ("_get_other_branch_names")
+            # 带有*的是当前分支
+            git branch | sed 's/^[ \t]*//' | grep -v '\*'
+            ;;
+        ("_get_branch_names")
+            # 基于_get_other_branch_names，拼接当前分支名称
+            echo "$(g _get_current_branch_name)"$'\n'"$(g _get_other_branch_names)"
+            ;;
+        ("_select_other_branch_name")
+            # 用xargs是为了去除开头的空格
+            # echo -n可以去除末尾的换行符
+            g _get_other_branch_names | sk | xargs echo -n
+            ;;
         ("_switch_to_target_branch")
             if [ -z "$2" ]; then
                 return
@@ -30,13 +43,18 @@ g(){
             # 切换到主分支
             g _switch_to_target_branch $(g _get_main_branch_name)
             ;;
+        ("_switch_to_other_branch_fzf")
+            # echo "fzf切换分支"
+            # 只能切换到其他分支
+            g _switch_to_target_branch $(g _select_other_branch_name)
+            ;;
         ("_delete_target_branch")
             # echo "删除指定分支，会连带远程分支一起删除"
             if [ -z "$2" ]; then
                 return
             fi
             TARGET_BRANCH=$2
-            git branch -D ${TARGET_BRANCH} && # 使用-D参数强制删除
+            git branch -D ${TARGET_BRANCH} && # 使用-D参数强制删除，避免提示报错
             git push origin --delete ${TARGET_BRANCH} &&
             git remote update origin --prune
             ;;
@@ -46,23 +64,23 @@ g(){
             g _switch_to_main_branch &&  # 先切换到主分支，因为不能删除当前分支
             g _delete_target_branch $CURRENT_BRANCH_NAME
             ;;
-        ("_select_branch_name")
-            # fzf切换分支
-            # sk是fzf的rust升级版
-            g b | sk
+        ("_delete_branch_fzf")
+            # echo "fzf删除分支"
+            # 默认只能删除其他分支，如果想要删除当前分支，应该用_delete_current_branch
+            g _delete_target_branch $(g _select_other_branch_name)
             ;;
         ("d")
-            # 如果没有$2，则删除当前分支
-            # 如果有$2，则删除$2
+            # 如果没有指定分支，则使用fzf选择分支
+            # 如果有指定分支，则删除指定分支
             if [ -z "$2" ]; then
-                g _delete_current_branch
+                g _delete_branch_fzf
             else
                 g _delete_target_branch $2
             fi
             ;;
         ("D")
-            # echo "fzf删除分支"
-            g _delete_target_branch $(g _select_branch_name)
+            # 删除当前分支
+            g _delete_current_branch
             ;;
         ("r")
             # echo "清空本地修改"
@@ -86,18 +104,22 @@ g(){
             # echo "所有分支"
             git branch # --all
             ;;
+        ("B")
+            # 展示所有分支，并通过fzf选中分支，最后复制分支名称
+            g _get_branch_names | sk | xargs echo -n | pbcopy
+            ;;
         ("s")
-            # 如果没有$2，则切换到主分支
-            # 如果有$2，则切换到$2
-            if [ -z "$2" ]; then 
-                g _switch_to_main_branch
+            # 如果没有指定分支，则使用fzf选择分支
+            # 如果有指定分支，则切换到指定分支
+            if [ -z "$2" ]; then
+                g _switch_to_other_branch_fzf
             else
                 g _switch_to_target_branch $2
             fi
             ;;
         ("S")
-            # echo "fzf切换分支"
-            g _switch_to_target_branch $(g _select_branch_name)
+            # 切换到主分支
+            g _switch_to_main_branch
             ;;
         ("f")
             # echo "强推到远程"
