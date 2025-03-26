13fb5e6542d3331064bdb8673aab58782f3e8006
Kaiyan-Zhang <zhangkaiyan3@gmail.com>
2025-03-27 01:06:42 +0800

    feat: fancy

--- fancy_zsh/main.sh
+++ fancy_zsh/main.sh
@@ -1,13 +1,13 @@
 source ${0:A:h}/zr.zsh
 
 # INSTALL FONT
-# brew install --cask font-meslo-lg-nerd-font
-# MesloLGM Nerd Font
+# brew install --cask font-maple-mono-nf-cn
+# Maple Mono NF CN
 
 # SET YOUR FONT
 # Apple Terminal: Terminal → Preferences → Profiles → Text
 # Hyper: 搜索fontFamily under module.exports.config
-# Vscode: "terminal.integrated.fontFamily": "MesloLGM Nerd Font"
-# GNOME Terminal: Terminal → Preferences and click on the selected profile under Profiles. Check Custom font under Text Appearance and select MesloLGS NF Regular.
+# Vscode: "terminal.integrated.fontFamily": "Maple Mono NF CN"
+# GNOME Terminal: Terminal → Preferences -> Profiles -> Text Appearance
 # Windows Terminal : search for: fontFace
 source ${0:A:h}/powerline.sh 
