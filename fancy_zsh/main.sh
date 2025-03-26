2c416fdacd1a4744f5f066d0dc1768d8a302dd52
Kaiyan-Zhang <zhangkaiyan3@gmail.com>
2025-03-27 01:11:34 +0800

    nice

--- fancy_zsh/main.sh
+++ fancy_zsh/main.sh
@@ -1,13 +1,19 @@
 source ${0:A:h}/zr.zsh
 
-# INSTALL FONT
+# INSTALL FONT: Maple Mono NF CN
 # brew install --cask font-maple-mono-nf-cn
-# Maple Mono NF CN
 
 # SET YOUR FONT
-# Apple Terminal: Terminal → Preferences → Profiles → Text
-# Hyper: 搜索fontFamily under module.exports.config
-# Vscode: "terminal.integrated.fontFamily": "Maple Mono NF CN"
-# GNOME Terminal: Terminal → Preferences -> Profiles -> Text Appearance
-# Windows Terminal : search for: fontFace
+# Apple Terminal:
+  # Terminal → Preferences → Profiles → Text
+# Hyper:
+  # 搜索fontFamily under module.exports.config
+# Vscode:
+  # "editor.fontFamily": "'Maple Mono NF CN'",
+  # "terminal.integrated.fontFamily": "'Maple Mono NF CN'",
+# GNOME Terminal:
+  # Terminal → Preferences -> Profiles -> Text Appearance
+# Windows Terminal:
+  # search for: fontFace
+
 source ${0:A:h}/powerline.sh 
