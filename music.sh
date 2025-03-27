bf414562caea8a4e3541c2b8d797a6d7eb422cd8
Kaiyan-Zhang <zhangkaiyan3@gmail.com>
2025-03-27 19:56:58 +0800

    cp

--- music.sh
+++ music.sh
@@ -10,3 +10,16 @@ h5(){
   fnm use 16.18.1 &&
   emo run start:hydev
 }
+
+case $(type pc) in
+   (*alias*) unalias pc;;
+esac
+pc(){
+  if [ $(basename $(pwd)) != "fe_douyin_music" ]; then
+      return;
+  fi
+
+  cd packages/music_storm_fe &&
+  fnm use 18 &&
+  emo run start
+}
